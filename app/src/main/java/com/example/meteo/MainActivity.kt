0cf87c6ac49717db2147a43e9bbed36ab868566c
Hari <harisaminada@gmail.com>
2025-04-20 18:48:19 +0200

    commit functionnable affichage

--- app/src/main/java/com/example/meteo/MainActivity.kt
+++ app/src/main/java/com/example/meteo/MainActivity.kt
@@ -1,26 +1,15 @@
-package com.example.testapp3almoc
+package com.example.meteo
 
 import android.os.Bundle
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.meteo.R
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mainActivityCentralTextView: TextView
-    private lateinit var topLeftTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-//FIND VIEW BY ID INTERDIT ICI
-        // INITIALISATION OF THE VIEW
+
+        // Lie le layout à l'activité
         setContentView(R.layout.activity_main)
-        this.mainActivityCentralTextView = findViewById(R.id.main_activity_central_text_tv) // crash
-        this.mainActivityCentralTextView.text = "New Text!"
-        this.topLeftTextView = findViewById(R.id.main_activity_second_text)
-        this.topLeftTextView.text = "Android c'est cool!"
     }
 }
